cb90843fabf19e33da29f4fdeae3ef058c17b3a5
devux <devakumar.mj@gmail.com>
2017-05-05 15:47:37 +0530

    Condition to remove unwanted instruction from user input

--- index.py
+++ index.py
@@ -1,12 +1,24 @@
-import turtle # module for Turtle graphics
+import turtle #module for Turtle graphics
+import re #module for Regular expression operations
 
-instructFilter = 'f,f,f,r,l,l,f,r'
+
+# Get Instruction from user
+print "Enter a Instruction with the following words: F,R,L \n",
+instruct = raw_input()
+
+#Removing unwanted string expect -flrFL and R
+instructFilter = re.sub(r'[^flrFLR]', '', instruct)
+
+print "Accepted string from your Input \n"
+print instructFilter
+instructArray = list(instructFilter)
 
 # Setting Up intial state of turtle
 turtle.clear()
 turtle.pensize(2)
 turtle.left(95)
 
+totalLength = len(instructArray)
 for index in range(len(instructArray)):
   if instructArray[index] == 'f':
 	  turtle.forward(1)
@@ -14,3 +26,7 @@ for index in range(len(instructArray)):
 	  turtle.right(90)
   elif instructArray[index] == 'l':
     turtle.left(95)
+
+  # Last cycle of for loop
+  if index == totalLength -1:
+		print turtle.pos()
