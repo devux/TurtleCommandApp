e6c7c8f51bddc3c21143a657d87d18d969933625
devux <devakumar.mj@gmail.com>
2017-05-05 15:42:41 +0530

    Recursive condition to move turtle

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 print "Welcome to turtle app"
+
+instructFilter = 'f,f,f,r,l,l,f,r'
+for index in range(len(instructArray)):
+  if instructArray[index] == 'f':
+	print "*** f"
+  elif instructArray[index] == 'r':
+	print "*** f"
+  elif instructArray[index] == 'l':
+    print "*** f"
