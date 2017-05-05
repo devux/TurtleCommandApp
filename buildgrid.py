b28fe93041e389dcc9267d80c8349bc4fad68015
devux <devakumar.mj@gmail.com>
2017-05-05 15:51:03 +0530

    Condition to build grid

---
+++ buildgrid.py
@@ -0,0 +1,14 @@
+# Initial setup
+board = []
+print "Enter N value to build grid and hurdles \n",
+grid_value = int(raw_input())
+
+# Construct n*n grid layer with n hurdles
+for row in range(grid_value):
+  board.append([])
+  random_value = random.randint(0,grid_value-1)
+  for column in range(grid_value):
+    if column == random_value & column != 0:
+      board[row].append('1')
+    else:
+      board[row].append('0')
